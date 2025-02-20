d8e489746c994610e08c11e5c6fef692024fd2ad
Ammarbinatif <raoammar11861@gmail.com>
2025-02-20 16:25:42 +0500

    Add files via upload

---
+++ practice Assign/app.js
@@ -0,0 +1,176 @@
+//alert ("hallo")//
+// Question1//
+ alert("Welcome To Our Website");
+
+
+// Question2//
+var message= ("Error! Please enter the valid password")
+alert(message);
+
+//Question3//
+var message=("Hallo \n bis spatter")
+alert(message);
+
+//Question4//
+var message1=("Guten morgen")
+var message2=("have a warm day")
+var message3=("enjoy your meal")
+alert(message1);
+alert(message2);
+alert(message3);
+
+//Question6//
+var message1=("hallo")
+var message2=("See my assignment")
+alert(message1);
+alert(message2);
+
+//Question7//
+var username= prompt("username")
+
+//Question8//
+var myName=prompt("Full Name")
+console.log(myName);
+
+//Question9//
+var message;
+message="Hello World"
+alert(message);
+
+//Question10//
+
+var nam = prompt(" your name")
+var gen= prompt("your gender")
+var age = prompt("Age")
+var study=prompt("qualification")
+var data= nam+ "\n" + gen+ "\n" + age+ "\n" + study
+alert(data);
+
+//Question11//
+
+var email="enter your email"
+alert(email);prompt("e.g. example@example.com")
+//---------Q13------- // 
+var emoji= "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(emoji);
+
+//Question14//
+
+var age;
+age=23
+alert(age);
+
+//Question16//
+var birthyear;
+birthyear=2001
+alert(birthyear);
+// Question17//
+
+var Name="John Doe "
+var pro= "baggy pants "
+var quan = 5  
+var info=(Name + "ordered " + quan + " " +pro+ "on OUTFITTER.com")
+alert(info);
+console.log(Name + "ordered " + quan + " " +pro+ "on OUTFITTER.com")
+
+//Question18//
+
+var a = 5,b=6,c=8;
+
+//Question19//
+//Legal Variables Names//
+var Geeks;
+var Geeks_for_Geeks;
+var $Geeks;
+var _Geeks;
+var Geeks123;
+
+//ILLEGAL Variables//
+var Geeks-for-Geeks; 
+var 123Geeks;  
+var Geeks for Geeks;  
+var @Geeks;
+var 2Geeks;
+
+//Question20//
+// //a
+document.write ("A heading stating Rules for naming JS variables")
+// //b
+document.write("<br/><br/> Variable names can only contain Digits, Letters,Underscore and Dollar signs. For example $my_1stVariable ")
+// //c
+document.write("<br/><br/>Variables must begin with a Digits,Letters or Dollar signs. For example$name, _name or name")
+// //d
+document.write("<br/><br/>Variable names are case Sensitive")
+// //e
+document.write("<br/><br/>Variable names should not be JS Reserved Words")
+
+
+//Question21//
+
+var numA=+prompt("Enter a First Number")
+var numB = +prompt("Enter a Second Number")
+var reslt = console.log(numA + numB);
+
+// Question22//
+
+var num1=+prompt("Enter a  Number for *")
+var num2 = +prompt("Enter a Another Number for *")
+var result1 = console.log(num1*num2)
+
+var num3=+prompt("Enter a Number for -")
+var num4 =+prompt("Enter a Second Number for -")
+var result2 = console.log(num3-num4)
+
+var num5=+prompt("Enter a First Number divide")
+var num6 =+prompt("Enter a Second Number for division")
+var result3 = console.log(num5/num6)
+
+//Question23//
+// //a 
+var a;
+// //b
+a=5
+console.log(a) 
+// // c
+a=5;
+// // d
+console.log(a)
+// // e
+a++
+// // f
+console.log(a)
+// // g
+var b= a+7;
+// // h
+console.log(b)
+// // i
+b--
+// // j
+console.log(b)
+// // k
+var c = b%3;
+// // l
+console.log(c)
+
+// Question24//
+
+var movie_ticket = 600 
+var costof_5 = 600*5
+console.log(costof_5)
+
+// Question 25//
+
+prompt("Enter any  number")
+document.write("<br/>Table of 2")
+var a = 2
+
+document.write("<br/>2x1 =" + a*1)
+document.write("<br/>2x2 =" + a*2)
+document.write("<br/>2x3 =" + a*3)
+document.write("<br/>2x4 =" + a*4)
+document.write("<br/>2x5 =" + a*5)
+document.write("<br/>2x6 =" + a*6)
+document.write("<br/>2x7 =" + a*7)
+document.write("<br/>2x8 =" + a*8)
+document.write("<br/>2x9 =" + a*9)
+document.write("<br/>2x10=" + a*10)
